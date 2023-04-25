8975a13df51be231a3d6c087e4ebe4f7553017f6
seth <107208142+socialsuicide@users.noreply.github.com>
2023-04-26 06:16:02 +1000

    Update Serverhop:Normal.lua

--- serverhop-library/examples/Serverhop:Normal.lua
+++ serverhop-library/examples/Serverhop:Normal.lua
@@ -1,1 +1,16 @@
+-- Auto collect orbs - Game: Legends Of Speed
 
+-- First we need to create a variable with the serverhop libary loadstring.
+local Serverhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/socialsuicide/lua-scripts/main/serverhop-library/src.lua"))();
+
+-- After that we index into the orb folder getting the descendants
+for i, v in pairs(game:GetService("Workspace").orbFolder.City:GetDescendants()) do
+    -- We check if the childs class is a TouchTransmitter
+    if v:IsA("TouchTransmitter") then
+        -- Then we fire the touch interest using are HumanoidRootPart on the item we wanna collect
+        firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
+    end
+end
+
+-- After collecting all orbs we wanna server hop
+Serverhop:Normal(true, 100) -- We put the first arg to true to enable logs. Then we put the second arg to 100 to scrape ~100 servers.
