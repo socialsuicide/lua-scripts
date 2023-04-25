a66e930195f4157014d64b0fd8daa02fc46f9c98
seth <107208142+socialsuicide@users.noreply.github.com>
2023-04-26 06:16:51 +1000

    Update Serverhop:Normal.lua

--- serverhop-library/examples/Serverhop:Normal.lua
+++ serverhop-library/examples/Serverhop:Normal.lua
@@ -5,12 +5,12 @@ local Serverhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/soc
 
 -- After that we index into the orb folder getting the descendants
 for i, v in pairs(game:GetService("Workspace").orbFolder.City:GetDescendants()) do
-    -- We check if the childs class is a TouchTransmitter
+    -- We then check if the childs class is a TouchTransmitter
     if v:IsA("TouchTransmitter") then
-        -- Then we fire the touch interest using are HumanoidRootPart on the item we wanna collect
+        -- If it is then we fire the touch interest using are HumanoidRootPart on the item we wanna collect
         firetouchinterest(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart, v.Parent, 0)
     end
 end
 
 -- After collecting all orbs we wanna server hop
-Serverhop:Normal(true, 100) -- We put the first arg to true to enable logs. Then we put the second arg to 100 to scrape ~100 servers.
+Serverhop:Normal(true, 100) -- We put the first arg to true to enable logs, then we put the second arg to 100 to scrape ~100 servers
