9d5d2527848c50b80ab9916d1d394deb39ab17e5
seth <107208142+socialsuicide@users.noreply.github.com>
2023-04-26 11:12:39 +1000

    Update Serverhop:Rejoin.lua

--- serverhop-library/examples/Serverhop:Rejoin.lua
+++ serverhop-library/examples/Serverhop:Rejoin.lua
@@ -1,1 +1,26 @@
+-- Auto gain strength - Game: Muscle Legends
 
+-- First we need to create a variable with the serverhop libary loadstring.
+local Serverhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/socialsuicide/lua-scripts/main/serverhop-library/src.lua"))();
+
+-- After that we create a thread to hold a infinite loop
+task.spawn(function()
+    -- Then we create a infinite loop
+    while task.wait() do
+        -- Then we check if the weight is not inside the players hand
+        if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("Weight") then
+            -- If not then we set the parent of the weight to the character
+            game:GetService("Players").LocalPlayer.Backpack:FindFirstChild("Weight").Parent = game:GetService("Players").LocalPlayer.Character
+        end
+    end
+end)
+
+-- After that we create a for loop
+for i = 1, 100 do
+    -- We then call the rep function getting us muscle
+    game:GetService("Players").LocalPlayer.muscleEvent:FireServer("rep")
+    task.wait(3)
+end
+
+-- After the for loop has ran 100 times we will rejoin the same server
+Serverhop:Rejoin(true) -- We put the first arg to true to enable logs
