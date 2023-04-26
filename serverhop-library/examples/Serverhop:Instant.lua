d66a56f88aa48d0f1960c1faf2b39b031914850b
seth <107208142+socialsuicide@users.noreply.github.com>
2023-04-26 13:32:46 +1000

    Update Serverhop:Instant.lua

--- serverhop-library/examples/Serverhop:Instant.lua
+++ serverhop-library/examples/Serverhop:Instant.lua
@@ -1,1 +1,14 @@
+-- Auto collect coins / Game: Ninja Legends
 
+-- First we need to create a variable with the serverhop libary loadstring.
+local Serverhop = loadstring(game:HttpGet("https://raw.githubusercontent.com/socialsuicide/lua-scripts/main/serverhop-library/src.lua"))();
+
+-- After that we need to index into the coins folder
+for i, v in pairs(game:GetService("Workspace").spawnedCoins.Valley:GetChildren()) do
+    -- We now need to collect this coin by teleporting to it
+    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+    task.wait(.5) 
+end
+
+-- After all the coins have been collected we will server hop
+Serverhop:Instant(true) -- We put the first arg to true to enable logs
